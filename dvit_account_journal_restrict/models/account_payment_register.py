b283abfaae15382481bc703fe43dd8c8aae265d7
Your Name <you@example.com>
2022-05-23 16:27:13 +0200

    [MOD]

--- dvit_account_journal_restrict/models/account_payment_register.py
+++ dvit_account_journal_restrict/models/account_payment_register.py
@@ -4,6 +4,6 @@ from odoo import fields, models, api
 class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
 
-    journal_id = fields.Many2one('account.journal', store=False, readonly=False,
+    journal_id = fields.Many2one('account.journal', store=True, readonly=False,
                                  compute=False,
                                  domain="[('company_id', '=', company_id), ('type', 'in', ('bank', 'cash'))]")
