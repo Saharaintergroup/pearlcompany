20b660779c8a10fc5aacfcd24f060800a590a9cc
Your Name <you@example.com>
2022-08-25 02:07:02 +0200

    [MOD]customer balance

---
+++ customer_balance/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Customer Balance",
+
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/account_invoice_report.xml',
+
+
+    ],
+
+}
